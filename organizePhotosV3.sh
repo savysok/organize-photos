fd50fae58d23f1009a1fc050977331d330e8f501
savysok <info@savysok.gr>
2016-02-12 15:18:54 +0200

    Updated script with 3 fixed issues

--- organizePhotosV3.sh
+++ organizePhotosV3.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+# Check to see if zenity and exiftool is installed
+# (copied from http://stackoverflow.com/questions/592620/check-if-a-program-exists-from-a-bash-script)
+type exiftool >/dev/null 2>&1 || { echo >&2 "This script requires exiftool but it's not installed.  Aborting."; }
+type zenity >/dev/null 2>&1 || { echo >&2 "This script requires zenity but it's not installed.  Aborting."; }
+
 # The directory where the photos are
+echo "Please select a source folder"
 SOURCE_DIR=$(zenity --file-selection --directory --title="SELECT A SOURCE FOLDER")
+echo "Source folder is: $SOURCE_DIR"
 
 # The destination directory
+echo "Please select the destination folder"
 DEST_DIR=$(zenity --file-selection --directory --title="SELECT A DESTINATION FOLDER")
+echo "Destination folder is: $DEST_DIR"
+
+zenity --info --text "Source folder is $SOURCE_DIR. Destination folder is $DEST_DIR"
 
 # Extra subfolder
 DEST_SUBFOLDER=$(zenity --entry --text="Name of subfolder" --title="Add the name of the subfolder (leave blank for none)")
@@ -18,8 +29,7 @@ EXTENSION=jpg
 EXTENSION_RAW=cr2
 
 # Move or copy the files
-#OPERATION=`zenity --entry --text="Would you like to move or copy the files? (mv for move, cp for copy)" --title="Method of operation"`
-OPERATION=$(zenity  --list  --text "Would you like to move or copy the files? (mv for move, cp for copy)" --radiolist  --column "Select" --column "Operation" TRUE cp FALSE mv)
+OPERATION=$(zenity --list --text "Would you like to move or copy the files?" --radiolist --column "Select" --column "Operation" TRUE Copy FALSE Move)
 
 #Operation for jpg images
 for f in $(find "$SOURCE_DIR" -iname "*.$EXTENSION" -type f); do
@@ -40,13 +50,17 @@ echo "Creating directory $f_dest_dir_final"
 mkdir "$f_dest_dir_final"
 fi
 
-$OPERATION "$f" "$f_dest_dir_final"
-
-if [ "$OPERATION" = cp ]; then
-echo "Copying $f to $f_dest_dir_final"
-else
-echo "Moving $f to $f_dest_dir_final"
-fi
+case $OPERATION in
+	"Copy")
+	echo "Copying $f to $f_dest_dir_final"
+	cp -Rv "$f" "$f_dest_dir_final"
+	;;
+	"Move")
+	echo
+	"Moving $f to $f_dest_dir_final"
+	mv -Rv "$f" "$f_dest_dir_final"
+	;;
+esac
 
 done
 
@@ -59,16 +73,19 @@ f_dest_dir_final_raw="$DEST_DIR/$f_date_raw/$DEST_SUBFOLDER/$DEST_SUBFOLDER_RAW"
 
 if [ ! -d "$f_dest_dir_final_raw" ]; then
 echo "Creating directory $f_dest_dir_final_raw"
-mkdir "$f_dest_dir_final_raw"
+mkdir -p "$f_dest_dir_final_raw"
 fi
 
-$OPERATION "$f_raw" "$f_dest_dir_final_raw"
-
-if [ "$OPERATION" = cp ]; then
-echo "Copying $f_raw to $f_dest_dir_final_raw"
-else
-echo "Moving $f_raw to $f_dest_dir_final_raw"
-fi
+case $OPERATION in
+	"Copy")
+	echo "Copying $f_raw to $f_dest_dir_final_raw"
+	cp -Rv "$f_raw" "$f_dest_dir_final_raw"
+	;;
+	"Move")
+	echo "Moving $f_raw to $f_dest_dir_final_raw"
+	mv -Rv "$f_raw" "$f_dest_dir_final_raw"
+	;;
+esac
 
 done
 
